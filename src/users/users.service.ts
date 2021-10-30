b6fbff748c9709acf2feef9929724bee7d7213b2
MarianaMSA <maiasalvador@gmail.com>
2021-10-29 21:50:08 -0300

    first commit

---
+++ src/users/users.service.ts
@@ -0,0 +1,19 @@
+import { Injectable, ConflictException } from '@nestjs/common';
+import { Prisma, User } from '@prisma/client'; //o User é o nome da tabela
+import { PrismaService } from 'src/prisma.service';
+import { UserRole } from 'src/enum/role.enum';
+
+@Injectable()
+export class UsersService {
+  constructor(private db: PrismaService) {}
+
+  async create(data: Prisma.UserCreateInput, role: UserRole): Promise<User> {
+    const userExists = await this.db.user.findUnique({
+      where: { email: data.email },
+    });
+
+    if (userExists) {
+      throw new ConflictException('Email já está cadastrado');
+    }
+  }
+}
